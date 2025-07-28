bde6ac48f525d9eae88e6d61a34c90e94c2046e3
Gagan deep <gaganbarhpagga@icloud.com>
2025-07-28 10:48:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,32 +19,3 @@ gsap.from(".hero p, #ourservices", {
   delay: 1
 });
 
-gsap.utils.toArray(".gallery-car").forEach((card, i) => {
-  gsap.from(card, {
-    scrollTrigger: {
-      trigger: card,
-      start: "top 90%",
-    },
-    opacity: 0,
-    y: 30,
-    duration: 1,
-    delay: i * 0.2,
-    ease: "power2.out"
-  });
-});
-
-gsap.utils.toArray(".helpdesk-card").forEach((card, i) => {
-  gsap.from(card, {
-    scrollTrigger: {
-      trigger: card,
-      start: "top 85%",
-    },
-    opacity: 0,
-    scale: 0.9,
-    duration: 1,
-    delay: i * 0.3,
-    ease: "back.out(1.7)"
-  });
-});
-
-
